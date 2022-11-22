ac9055c7726fe9df3ef924689d9b60eda73ae248
Waqas khan <thewaqas1@hotmail.com>
2022-11-22 18:59:05 +0500

    ProductsPage

---
+++ ECommerceProject.Web/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceProject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceProject.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            CategoryService categoryService = new CategoryService();
+            var categoriesList = categoryService.getCategories();
+
+            return View(categoriesList);
+        }
+    }
+}
