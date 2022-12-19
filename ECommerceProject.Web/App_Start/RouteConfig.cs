795386fa56946d024b255308b6ee2bb6c6a8d318
Waqas khan <thewaqas1@hotmail.com>
2022-12-19 18:34:45 +0500

    Route

--- ECommerceProject.Web/App_Start/RouteConfig.cs
+++ ECommerceProject.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace ECommerceProject.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "NewCategory",
+                url: "new/cat",
+                defaults: new { controller = "Category", action = "Create", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "AllCategory",
+                url: "searchCategory/all",
+                defaults: new { controller = "Category", action = "CategoryTable", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
